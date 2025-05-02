79b3f47db6dfb13c0967ca727895584bf2b78bdd
WuTangLan1 <finnmassari16@gmail.com>
2025-05-02 23:38:56 +0200

    added overlayload

--- app.vue
+++ app.vue
@@ -1,6 +1,39 @@
 <!-- app.vue -->
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import OverlayLoad from '~/components/overlayLoad.vue'
+
+const showOverlay = ref(true)
+
+onMounted(() => {
+  setTimeout(() => {
+    showOverlay.value = false
+  }, 2000)
+})
+</script>
+
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <transition name="fade" mode="out-in" v-if="showOverlay" key="overlay">
+    <OverlayLoad />
+  </transition>
+  <transition name="fade" mode="out-in" v-else key="layout">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </transition>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+</style>
---
+++ components/overlayLoad.vue
@@ -0,0 +1,8 @@
+<!-- components/overlayLoad.vue -->
+<template>
+    <div class="fixed inset-0 bg-white dark:bg-gray-900 flex items-center justify-center z-[9999]">
+      <div class="animate-spin h-16 w-16 border-4 border-blue-500 dark:border-blue-300 border-t-transparent rounded-full"></div>
+    </div>
+  </template>
+  <script setup lang="ts"></script>
+  
