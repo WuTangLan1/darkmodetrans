ac75cddefd31fd7c1569954191594feae69c7dd6
WuTangLan1 <finnmassari16@gmail.com>
2025-05-02 14:35:04 +0200

    added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
  // nuxt.config.ts
  export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }
+      ]
+    }
+  },
   compatibilityDate: '2025-05-01',
   modules: [
     '@nuxtjs/tailwindcss'
